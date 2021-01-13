fd9aa7c8ee991de29eb610df3d8a69dbaa8ada05
NBender8668 <61422838+NBender8668@users.noreply.github.com>
2021-01-12 18:56:19 -0700

    Initial Project
    Initial Project Assests Commit

---
+++ Assets/Scripts/Crate.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Crate : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision collision)
+    {
+        AudioSource audioSource = GetComponent<AudioSource>();
+        audioSource?.Play();
+        Debug.Log(collision.gameObject);
+    }
+}
---
+++ Assets/Scripts/Factory.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Factory : MonoBehaviour
+{
+
+    [SerializeField] GameObject prefab;
+
+  
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            GameObject gameObject = Instantiate(prefab,this.transform,true);
+            Destroy(gameObject, 1);
+        }
+    }
+}
